a053317a7982fdcba6b9dadac88f99397697af40
Spencer Croy <spencerccroy@gmail.com>
2018-10-14 19:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy (n) {
   const rlist = document.querySelectorAll('.ranked-list');
 
   for (let i = 0; i < rlist.length; i++) {
-    console.log(rlist[i].innerHTML)
+    console.log(rlist[i].innerHTML + n)
     //rlist[i].innerHTML = parseInt(rlist[i].innerHTML) + parseInt(n);
   }
   return;
