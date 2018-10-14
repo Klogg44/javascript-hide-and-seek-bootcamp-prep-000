d926c94ce6d5d876384c7fc448fc478048087464
Spencer Croy <spencerccroy@gmail.com>
2018-10-14 19:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function nestedTarget () {
 }
 
 function increaseRankBy (n) {
-  const rlist = document.querySelectorAll('.ranked-list');
+  const rlist = document.querySelectorAll('.ranked-list li');
 
   for (let i = 0; i < rlist.length; i++) {
-    console.log((parseInt(rlist[i].innerHTML) + n))
+    console.log(rlist[i].innerHTML)
     //rlist[i].innerHTML = parseInt(rlist[i].innerHTML) + parseInt(n);
   }
   return;
